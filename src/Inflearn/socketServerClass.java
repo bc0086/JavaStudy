f84a49f651412c84c355baa1726b5891fa7f0952
bc0086 <bc0086@naver.com>
2020-10-10 03:43:00 +0900

    2020-10-10 03:42 h

--- src/Inflearn/socketServerClass.java
+++ src/Inflearn/socketServerClass.java
@@ -9,7 +9,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class serverClass {
+public class socketServerClass {
 
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
